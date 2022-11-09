83c1bb9e148df34e78c15d7da52278185a5e8c88
Leetzner <ognill@hotmail.com>
2022-11-09 10:14:34 +0000

    added a function for random word to start the game

--- run.py
+++ run.py
@@ -2,13 +2,8 @@
 import random
 from words import word_list
 
+
 def start_word():
     word = random.choice(word_list)
     return word.upper()
 
-def play(word):
-    word_completiton="_" * len(word)
-    guessed = False
-    guessed_letters = []
-    guessed_words = []
-    tries = 7
