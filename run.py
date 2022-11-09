20d35d3102019c98f13e06ed03c52c89bbf7bb2e
Leetzner <ognill@hotmail.com>
2022-11-09 10:18:34 +0000

    added code-structure to build the game on

--- run.py
+++ run.py
@@ -2,8 +2,20 @@
 import random
 from words import word_list
 
-
+#This function handles the random choice of words from the wordslist
 def start_word():
     word = random.choice(word_list)
     return word.upper()
 
+def play(word):
+    word_completiton="_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 7
+    print("I challenge you to not get hanged by the pole")
+    print(display_hangman(tries))
+    print(word_completiton)
+    print("/n")
+    while not guessed and tried > 0 :
+        guess = input("Please guess a letter or word:").upper()
