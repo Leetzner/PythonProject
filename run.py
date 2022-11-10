2e77ed0baeff28392f2372464f83c6450bc230ba
Leetzner <ognill@hotmail.com>
2022-11-10 10:37:42 +0000

    added a function to restart the game if you won or lost

--- run.py
+++ run.py
@@ -57,20 +57,87 @@ def play(word):
         
 def display_hangman(tries):
     stages = [  
-                # initial empty state
+                #final state, head, torso, both arms, both legs and dead-face
+                """
+                   --------
+                   |      |   
+                   |    (x.x) 
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+        
+        
+                #head, torso, both arms, and both legs
+                """
+                   --------
+                   |      |    
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                # head, torso, both arms, and one leg
                 """
                    --------
                    |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # head, torso, and both arms
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
                    |      
+                   -
+                """,
+                # head, torso, and one arm
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                """,
+                # head and torso
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # head
+                """
+                   --------
+                   |      |
+                   |      O
                    |    
                    |      
                    |     
                    -
-                """
+                """,
     ]
     return stages[tries]
 
-
+def main():
+    word = get_word()
+    play(word)
+    while input("Wanna go again? (Y for yes/N for No)").upper() == "Y":
+        word = get_word()
+        play (word)
         
 
     
