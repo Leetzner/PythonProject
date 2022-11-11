d1450aa91daceb6fed6825c4974fe5d4c64a32a1
Leetzner <ognill@hotmail.com>
2022-11-11 13:17:22 +0000

    did some misspelling on the word completion, changed it now the game works

--- run.py
+++ run.py
@@ -8,73 +8,60 @@ def start_word():
     return word.upper()
 
 def play(word):
-    word_completiton="_" * len(word)
+    word_completion = "_" * len(word)
     guessed = False
     guessed_letters = []
     guessed_words = []
-    tries = 7
+    tries = 6
     print("I challenge you to NOT get hanged by the pole""/n""Good luck!")
     print(display_hangman(tries))
-    print(word_completiton)
+    print(word_completion)
     print("/n")
 #While loop for handling the ammount of tries and when you´re out of guesses
-    while not guessed and tried > 0 :
+    while not guessed and tries > 0 :
         guess = input("Please guess a letter or word:").upper()
         if len(guess) == 1 and guess.isalpha():
             if guess in guessed_letters:
-                print("Haven´t you clicked here before?", guess)
+                print("Havent you clicked here before?", guess)
             elif guess not in word:
-                print(guess, "it´s a good guess, but not the right one")
+                print(guess, "its a good guess, but not the right one")
                 tries  -= 1
                 guessed_letters.append(guess)
             else:
-                print("You are right!" guess, "is in the word!")
+                print("You are right!", guess, "is in the word!")
                 guessed_letters.append(guess)
-                word_as_list = list(word_completiton)
+                word_as_list = list(word_completion)
                 indices = [i for i, letter in enumerate(word) if letter == guess]
                 for index in indices:
                     word_as_list[index] = guess
-                word_completiton = "".join(word_as_list)  
-                if"_" not in word_completiton: 
+                word_completion = "".join(word_as_list)  
+                if"_" not in word_completion: 
                     guess = True
         elif len(guess) == len(word) and guess.isalpha():
             if guess in guessed_words:
                 print("You already clicked", guess)
             elif guess != word:
-                print("the letter" guess, "is not in the word.")
+                print("the letter", guess, "is not in the word.")
                 tries -= 1
                 guessed_words.append(guess)
             else:
                 guessed = True
-                word_completiton = word
+                word_completion = word
         else:
             print("Not a valid guess.")
         print(display_hangman(tries))
-        print(word_completiton)
+        print(word_completion)
         print("/n")
     if guessed:
-        print("You´re a mastermind, you got the whole thing right! Well done! Up for another one?")
+        print("Youre a mastermind, you got the whole thing right! Well done! Up for another one?")
     else:
-        print("How many tries left? that´s right, NONE! Come back when you looked in the dictionary!")
+        print("How many tries left? thats right, NONE! Come back when you have looked in the dictionary! This word was " + word + "")
         
 def display_hangman(tries):
-    stages = [  
-                #final state, head, torso, both arms, both legs and dead-face
+    stages = [  # final state: head, torso, both arms, and both legs
                 """
                    --------
-                   |      |   
-                   |    (x.x) 
-                   |     \\|/
                    |      |
-                   |     / \\
-                   -
-                """,
-        
-        
-                #head, torso, both arms, and both legs
-                """
-                   --------
-                   |      |    
                    |      O
                    |     \\|/
                    |      |
@@ -131,16 +118,25 @@ def display_hangman(tries):
                    |     
                    -
                 """,
+                # initial empty state
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
     ]
     return stages[tries]
 
 def main():
-    word = get_word()
+    word = start_word()
     play(word)
     while input("Wanna go again? (Y for yes/N for No)").upper() == "Y":
-        word = get_word()
+        word = start_word()
         play (word)
-        
 
-    
-            
+if __name__ == "__main__":
+    main()
